13df10d46b231c055da7049e1f4e21fc38c64d02
Naitik Bansal <labsconnectbusiness@gmail.com>
2025-05-26 22:33:34 +0530

    Add files via upload

---
+++ CheckLargerNumber.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+
+int a;
+int b;
+
+ cout<<"Enter number 1 ";
+ cin>>a;
+
+ cout<<"Enter number 2 ";
+ cin>>b;
+
+ if (a>b){
+
+    cout<<"Number 1 is bigger";
+
+ } 
+ 
+ else if(a==b){
+   cout<<"Both numbers are equal!!";
+ }  
+ 
+ else {
+
+    cout<<"Number 2 is bigger";
+
+}
+
+return 0;
+
+}
